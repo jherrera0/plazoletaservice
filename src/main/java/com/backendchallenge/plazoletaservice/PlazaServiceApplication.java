f323de054452a14a6052d14cbaea019774c8399b
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-28 14:45:17 -0500

    initial commit

---
+++ src/main/java/com/backendchallenge/plazoletaservice/PlazaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backendchallenge.plazoletaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendchallenge/plazoletaservice/PlazaServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.backendchallenge.plazoletaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlazaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//verify that the application context loads successfully
+	}
+
+}
