15ce2ee683fd03a42f0370ecab587948c4d5e5a4
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 14:17:35 -0500

    ADD documentation

---
+++ src/main/java/com/backendchallenge/plazoletaservice/domain/until/ConstDocumentation.java
@@ -0,0 +1,16 @@
+package com.backendchallenge.plazoletaservice.domain.until;
+
+public class ConstDocumentation {
+
+
+    public static final String RESTAURANT_TAG_NAME = "Restaurant Rest API";
+    public static final String RESTAURANT_TAG_DESCRIPTION = "API to manage restaurant creation requests";
+    public static final String CODE_400 = "400";
+    public static final String CODE_201 = "201";
+    public static final String CREATE_RESTAURANT_OPERATION = "Create a new restaurant";
+    public static final String CREATE_RESTAURANT_CODE_400 = "Invalid data to request create restaurant";
+    public static final String CREATE_RESTAURANT_CODE_201 = "Restaurant created successfully";
+
+    private ConstDocumentation() {
+    }
+}
