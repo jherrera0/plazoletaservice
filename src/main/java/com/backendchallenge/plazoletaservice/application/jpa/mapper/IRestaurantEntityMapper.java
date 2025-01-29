ab6aeb42a5ecd617bf6ef6ed28cacbf923d272be
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 14:12:33 -0500

    correct mapping error

--- src/main/java/com/backendchallenge/plazoletaservice/application/jpa/mapper/IRestaurantEntityMapper.java
+++ src/main/java/com/backendchallenge/plazoletaservice/application/jpa/mapper/IRestaurantEntityMapper.java
@@ -3,6 +3,7 @@ package com.backendchallenge.plazoletaservice.application.jpa.mapper;
 import com.backendchallenge.plazoletaservice.application.jpa.entity.RestaurantEntity;
 import com.backendchallenge.plazoletaservice.domain.model.Restaurant;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper (componentModel = "spring",
@@ -10,5 +11,6 @@ import org.mapstruct.ReportingPolicy;
         unmappedSourcePolicy = ReportingPolicy.IGNORE)
 
 public interface IRestaurantEntityMapper {
+    @Mapping(target = "id", ignore = true)
     RestaurantEntity toEntity(Restaurant restaurant);
 }
