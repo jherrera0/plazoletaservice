1ba68531851f1e0f20e96bd2b2595605329bae5b
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 15:11:04 -0500

    add missing jakarta validation

--- src/main/java/com/backendchallenge/plazoletaservice/application/http/dto/CreateDishRequest.java
+++ src/main/java/com/backendchallenge/plazoletaservice/application/http/dto/CreateDishRequest.java
@@ -1,15 +1,28 @@
 package com.backendchallenge.plazoletaservice.application.http.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class CreateDishRequest {
+    @Positive
     private Long idRestaurant;
+    @NotBlank
+    @NotNull
     private String name;
+    @Positive
     private Integer price;
+    @NotBlank
+    @NotNull
     private String description;
+    @NotBlank
+    @NotNull
     private String urlImage;
+    @NotBlank
+    @NotNull
     private String category;
 }
