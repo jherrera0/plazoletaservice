4aae7b1cae4efbabc1bd139ec5228a7de1e05719
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 17:09:39 -0500

    add jakarta validation dependencies and exception controller

--- src/main/java/com/backendchallenge/plazoletaservice/infrastructure/configuration/excepcionhandler/DishExceptionHandler.java
+++ src/main/java/com/backendchallenge/plazoletaservice/infrastructure/configuration/excepcionhandler/DishExceptionHandler.java
@@ -1,10 +1,19 @@
 package com.backendchallenge.plazoletaservice.infrastructure.configuration.excepcionhandler;
 
 import com.backendchallenge.plazoletaservice.domain.exceptions.dishexceptions.*;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class DishExceptionHandler  extends ResponseEntityExceptionHandler {
@@ -32,5 +41,19 @@ public class DishExceptionHandler  extends ResponseEntityExceptionHandler {
     @ExceptionHandler(DishPriceInvalidValueException.class)
     public ResponseEntity<Object> handleDishPriceInvalidValueException(DishPriceInvalidValueException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  @NonNull HttpHeaders headers,
+                                                                  @NonNull HttpStatusCode status,
+                                                                  @NonNull WebRequest request) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
--- src/main/java/com/backendchallenge/plazoletaservice/infrastructure/configuration/excepcionhandler/RestaurantExceptionHandler.java
+++ src/main/java/com/backendchallenge/plazoletaservice/infrastructure/configuration/excepcionhandler/RestaurantExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.backendchallenge.plazoletaservice.infrastructure.configuration.excepcionhandler;
 
 import com.backendchallenge.plazoletaservice.domain.exceptions.restaurantexceptions.*;
+import lombok.NonNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class RestaurantExceptionHandler extends ResponseEntityExceptionHandler {
@@ -65,4 +73,17 @@ public class RestaurantExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  @NonNull HttpHeaders headers,
+                                                                  @NonNull HttpStatusCode status,
+                                                                  @NonNull WebRequest request) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
