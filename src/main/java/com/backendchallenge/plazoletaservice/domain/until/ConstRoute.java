931d18de71338e9423b990b0889019881e386fb2
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 18:34:18 -0500

    implement logic on the infrastructure layer

--- src/main/java/com/backendchallenge/plazoletaservice/domain/until/ConstRoute.java
+++ src/main/java/com/backendchallenge/plazoletaservice/domain/until/ConstRoute.java
@@ -6,6 +6,7 @@ public class ConstRoute {
     public static final String CREATE_RESTAURANT = "/CREATE_RESTAURANT";
     public static final String DISH = "/dish";
     public static final String CREATE_DISH = "/CREATE_DISH";
+    public static final String UPDATE_DISH = "/UPDATE_DISH";
 
     private ConstRoute() {
     }
