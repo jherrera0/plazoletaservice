cfb91ed52d04859784467ba164f0f7b62e5ecee0
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-30 15:16:56 -0500

    implement the unit tests of authentication service

---
+++ src/main/java/com/backendchallenge/plazoletaservice/infrastructure/configuration/security/filter/JwtService.java
@@ -0,0 +1,29 @@
+package com.backendchallenge.plazoletaservice.infrastructure.configuration.security.filter;
+
+import com.backendchallenge.plazoletaservice.domain.until.ConstJwt;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.security.Key;
+
+public class JwtService {
+    @Value("${app-security-key}")
+    private String secretKey = "mysecretkeymysecretkeymysecretkeymy";
+
+    public String extractUsername(String jwt) {
+        return extractAllClaims(jwt).getSubject();
+    }
+
+    public String extractRole(String jwt) {
+        return extractAllClaims(jwt).get(ConstJwt.ROLE).toString();
+    }
+
+    public Claims extractAllClaims(String jwt) {
+        return Jwts.parserBuilder().setSigningKey(generateKey()).build().parseClaimsJws(jwt).getBody();
+    }
+
+    Key generateKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
