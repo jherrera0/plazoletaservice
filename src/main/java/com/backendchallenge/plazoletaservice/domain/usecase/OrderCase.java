ad40a3b29c332193327e14f3cf9bfcee5f859e81
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-03 06:30:50 -0500

    change logic on the domain layer

--- src/main/java/com/backendchallenge/plazoletaservice/domain/usecase/OrderCase.java
+++ src/main/java/com/backendchallenge/plazoletaservice/domain/usecase/OrderCase.java
@@ -5,26 +5,33 @@ import com.backendchallenge.plazoletaservice.domain.exceptions.orderexceptions.*
 import com.backendchallenge.plazoletaservice.domain.exceptions.restaurantexceptions.RestaurantNotFoundException;
 import com.backendchallenge.plazoletaservice.domain.model.Order;
 import com.backendchallenge.plazoletaservice.domain.spi.IDishPersistencePort;
+import com.backendchallenge.plazoletaservice.domain.spi.IJwtPersistencePort;
 import com.backendchallenge.plazoletaservice.domain.spi.IOrderPersistencePort;
 import com.backendchallenge.plazoletaservice.domain.spi.IRestaurantPersistencePort;
 import com.backendchallenge.plazoletaservice.domain.until.ConstValidation;
+import com.backendchallenge.plazoletaservice.domain.until.TokenHolder;
 
 public class OrderCase implements IOrderServicePort {
 
     private final IOrderPersistencePort orderPersistencePort;
     private final IRestaurantPersistencePort restaurantPersistencePort;
     private final IDishPersistencePort dishPersistencePort;
+    private final IJwtPersistencePort jwtPersistencePort;
 
     public OrderCase(IOrderPersistencePort orderPersistencePort,
                      IRestaurantPersistencePort restaurantPersistencePort,
-                     IDishPersistencePort dishPersistencePort) {
+                     IDishPersistencePort dishPersistencePort,
+                     IJwtPersistencePort jwtPersistencePort) {
         this.orderPersistencePort = orderPersistencePort;
         this.restaurantPersistencePort = restaurantPersistencePort;
         this.dishPersistencePort = dishPersistencePort;
+        this.jwtPersistencePort = jwtPersistencePort;
     }
 
     @Override
     public void createOrder(Order order) {
+        String token = TokenHolder.getTokenValue();
+        order.setIdClient(jwtPersistencePort.getUserId(token));
         if (orderPersistencePort.findOrderByClientId(order.getIdClient())) {
             throw new OrderAlreadyExistsException();
         }
