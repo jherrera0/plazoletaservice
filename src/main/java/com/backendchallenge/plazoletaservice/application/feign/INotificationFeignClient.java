b9a763164309ebcf559ff385a8f6d1dbd9460ab6
Jose Herrera <jose.herrera@pragma.com.co>
2025-02-04 15:04:56 -0500

    Change logic on the application layer

--- src/main/java/com/backendchallenge/plazoletaservice/application/feign/INotificationFeignClient.java
+++ src/main/java/com/backendchallenge/plazoletaservice/application/feign/INotificationFeignClient.java
@@ -3,17 +3,17 @@ package com.backendchallenge.plazoletaservice.application.feign;
 import com.backendchallenge.plazoletaservice.application.http.dto.request.NotificationRequest;
 import com.backendchallenge.plazoletaservice.infrastructure.configuration.feign.FeignClientsConfig;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "notification-service", url = "http://localhost:8082", configuration= FeignClientsConfig.class)
 public interface INotificationFeignClient {
+
     @PostMapping("/notification/send")
     void sendNotification(NotificationRequest notificationRequest);
 
-    @GetMapping("/notification/getPin")
+    @PostMapping("/notification/getPin")
     String findPinByPhoneNumber(String phone);
 
-    @GetMapping("/notification/exist")
+    @PostMapping("/notification/exist")
     Boolean existPinByPhoneNumber(String phone);
 }
